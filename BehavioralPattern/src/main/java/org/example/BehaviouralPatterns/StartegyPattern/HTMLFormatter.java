f2d269954031b943d2468ee9c1a892afedb2d984
Santhosh Puvaneswaran <64911148+sandyhandle@users.noreply.github.com>
2025-07-05 14:45:56 +0530

    HTMLFormatter file typo is corrected. (#1)
    Co-authored-by: sandyhandle <ranjith.puvaneswaran@gmail.com>

--- BehavioralPattern/src/main/java/org/example/BehaviouralPatterns/StartegyPattern/HTMLFormatter.java
+++ BehavioralPattern/src/main/java/org/example/BehaviouralPatterns/StartegyPattern/HTMLFormatter.java
@@ -5,6 +5,6 @@ public class HTMLFormatter implements TextFormatter {
     @Override
     public String format(String text) {
         // TODO: Wrap the input text in HTML tags: "<html><body>" and "</body></html>".
-        return "<html><body>" +text+"<html><body>";
+        return "<html><body>" +text+"</body></html>";
     }
 }
